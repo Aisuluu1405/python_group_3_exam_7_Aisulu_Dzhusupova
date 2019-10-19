8d426749c64fcc0951d4c8e6542e5f0640a9c7c6
Айсулу <aisuluueco2009@yandex.ru>
2019-10-19 12:29:48 +0600

    Зарегистрировала модели в админ-панели и добавила данные, создала фикстуру.

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from webapp.models import Poll, Choice
 
-# Register your models here.
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'question', 'created']
+    list_display_links = ['pk', 'question']
+    exclude = []
+
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice)
