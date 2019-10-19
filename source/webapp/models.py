ca93d22b1b0d4e7c297cff0cb3be4d148285f5af
Айсулу <aisuluueco2009@yandex.ru>
2019-10-19 12:08:01 +0600

    Создала модели 'Poll','Choice'.

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    question = models.TextField(max_length=3000, verbose_name='Question')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Date of creation')
+
+    def __str__(self):
+        return self.question
+
+
+class Choice(models.Model):
+    text = models.TextField(max_length=2000, verbose_name='Choice')
+    poll = models.ForeignKey('webapp.Poll', related_name='choices',
+                             on_delete=models.CASCADE, verbose_name='Poll')
+
+    def __str__(self):
+        return self.text
